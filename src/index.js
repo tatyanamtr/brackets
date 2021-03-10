9f80cc104b38a9797b8fcc0c2c65083b436f7c1d
tatyanamtr <tatyana.a.mtr@gmail.com>
2021-03-10 20:50:13 +0300

    feat: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let arr = str.split("");
+  let n = 0;
+  for ( let i = 0; i < arr.length; i++ ) {
+  	if (arr[i] == "(" || arr[i] == "[" || arr[i] == "{") n++;
+  	if (arr[i] == ")" || arr[i] == "]" || arr[i] == "}") n--;
+  	if (n<0) return false;
+  }
+  return n==0;
 }
